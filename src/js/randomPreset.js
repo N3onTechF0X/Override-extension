fba53f218c01988eba83a658e1b81ce1ca65c3e6
N3onTechF0X <neontechfox@gmail.com>
2025-06-24 18:33:10 +0300

    .

---
+++ src/js/randomPreset.js
@@ -0,0 +1,12 @@
+document.getElementById('randomPreset').addEventListener('click', function() {
+    const selects = document.querySelectorAll('select');
+    selects.forEach(select => {
+        const options = Array.from(select.options);
+        const randomIndex = 1 + Math.floor(Math.random() * (options.length - 1));
+        const randomValue = options[randomIndex].value;
+
+        select.value = randomValue;
+        selections[select.id] = randomValue;
+    });
+    savePreset();
+});
